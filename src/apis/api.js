19aeaabc95539446c60981fc17bea13d649b882a
gomja <kategom@snu.ac.kr>
2026-05-23 22:07:16 +0900

    week10-dk-hw

--- src/apis/api.js
+++ src/apis/api.js
@@ -72,7 +72,21 @@ export const deletePost = async (id, navigate) => {
 };
 
 // 과제!!
-export const likePost = async (postId) => {};
+export const likePost = async (postId) => {
+  try {
+    const response = await instanceWithToken.post(`/post/${postId}/like/`, {});
+
+    if (response.status === 200 || response.status === 201) {
+      console.log("LIKE SUCCESS");
+      window.location.reload(); // 좋아요 수 카운트를 화면에 반영하기 위해 새로고침
+    } else {
+      console.log("[ERROR] error while liking post");
+    }
+  } catch (error) {
+    console.error("좋아요 API 호출 중 에러 발생:", error);
+    throw error;
+  }
+};
 
 // Tag 관련 API들
 export const getTags = async () => {
@@ -116,5 +130,12 @@ export const updateComment = async (id, data) => {
   }
 };
 
-// 과제 !!
-export const deleteComment = async (id) => {};
+export const deleteComment = async (id) => {
+  const response = await instanceWithToken.delete(`/comment/${id}/`);
+  if (response.status === 204 || response.status === 200) {
+    console.log("COMMENT DELETE SUCCESS");
+    window.location.reload(); // 삭제 후 새로고침으로 반영
+  } else {
+    console.log("[ERROR] error while deleting comment");
+  }
+};
