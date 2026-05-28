be911e275347169a23da4672a0295290caccb3e5
yanghyeonseo <yanghyeonseo1018@gmail.com>
2026-05-28 09:48:38 +0900

    seminar ended

--- src/routes/SignUpPage.jsx
+++ src/routes/SignUpPage.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import axios from "axios";
+import { getCookie } from "../utils/cookie";
+import { signUp } from "../apis/api";
 
 const SignUpPage = () => {
   const [signUpData, setSignUpData] = useState({
@@ -15,10 +18,9 @@ const SignUpPage = () => {
     setSignUpData({ ...signUpData, [id]: value });
   };
 
-  const handleSignUpSubmit = (e) => {
+  const handleSignUpSubmit = async (e) => {
     e.preventDefault(); // to prevent reloading the page
-    console.log(signUpData);
-    alert("회원가입 하기"); // TODO: add api call for sign up
+    signUp(signUpData);
   };
 
   return (
@@ -32,8 +34,8 @@ const SignUpPage = () => {
           required
           type="email"
           id="email"
-          className="input" 
-          value={signUpData.email} 
+          className="input"
+          value={signUpData.email}
           onChange={handleSignUpData}
         />
 
@@ -60,7 +62,6 @@ const SignUpPage = () => {
           value={signUpData.password}
           onChange={handleSignUpData}
         />
-
 
         <label htmlFor="confirm_password" className="label">
           *비밀번호 확인:
